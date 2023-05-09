0b60936f9422d7c7808aae99830535aea091cebe
Vadim Vetrov <vetrovvd@gmail.com>
2023-05-09 15:58:22 +0300

    Disabled request caching which make an application more stateless (but still it is using sessions)

--- src/main/kotlin/org/amfibot/auth/config/DefaultSecurityConfig.kt
+++ src/main/kotlin/org/amfibot/auth/config/DefaultSecurityConfig.kt
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.savedrequest.NullRequestCache
 
 
 @Configuration
@@ -35,9 +36,13 @@ class DefaultSecurityConfig {
                     userAuthoritiesMapper = userAuthoritiesMapper()
                 }
             }
+
+            requestCache {
+                requestCache = NullRequestCache()
+            }
         }
 
-
+        
         return http.build()
     }
 
