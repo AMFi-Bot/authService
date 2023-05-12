91ee0b61ca4b8867c844b43e7b7e481cbb35902e
Vadim Vetrov <vetrovvd@gmail.com>
2023-05-12 22:48:34 +0300

    Decided to use sessions in authorization server

--- src/main/kotlin/org/amfibot/auth/config/DefaultSecurityConfig.kt
+++ src/main/kotlin/org/amfibot/auth/config/DefaultSecurityConfig.kt
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.savedrequest.NullRequestCache
 
 
 @Configuration
@@ -36,13 +35,9 @@ class DefaultSecurityConfig {
                     userAuthoritiesMapper = userAuthoritiesMapper()
                 }
             }
-
-            requestCache {
-                requestCache = NullRequestCache()
-            }
         }
 
-        
+
         return http.build()
     }
 
